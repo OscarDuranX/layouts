be1225dd9b619c446254846ae37b0f9d6a0f12c6
Oscar Duran <xoscarduranx@gmail.com>
2017-01-24 16:02:35 +0100

    final de install

--- routes/web.php
+++ routes/web.php
@@ -14,3 +14,16 @@
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::group(['middleware' => 'auth'], function () {
+    //    Route::get('/link1', function ()    {
+//        // Uses Auth Middleware
+//    });
+
+    //Please do not remove this if you want adminlte:route and adminlte:link commands to works correctly.
+    #adminlte_routes
+    Route::get('vertical', 'VerticalController@index')->name('vertical');
+
+    Route::get('prova', 'ProvaController@index')->name('prova');
+
+});
